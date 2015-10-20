3bda501b97062f6cc3b0cd8cd350b1ed1a28f1ff
Joshua Krage <jkrage@guisarme.us>
2015-10-20 12:55:56 -0400

    Use test-harness.sh common script

--- tests/test_helpers.sh
+++ tests/test_helpers.sh
@@ -2,34 +2,8 @@
 ###
 ### test_helpers.sh -- Tests for convenience functions and setup
 
-# Load the helpers.sh file
-source "$(dirname $0)/../helpers.sh" || { echo "ERROR: helpers.sh not found!" ;exit 1 ; }
-# Enforce being in the tests directory
-[[ "$(dirname $0)" == "." ]] || { echo "ERROR: Please run this script directly from the tests/ directory." ; exit 1 ; }
-
-### Master wrapper function for Testing
-function test_wrapper () {
-    local _INVERT=""
-    if [ "$1" == "--invert" ]; then
-        _INVERT="true"
-        shift
-    fi
-
-    local TEST_LABEL=$1
-    local TEST_FUNCTION=$2
-    local PRESERVE_OUTPUT=$3
-    local OUTPUT_OPTION=""
-
-    debug "Testing: ${TEST_LABEL} using ${TEST_FUNCTION}"
-    if [ ! -z "${PRESERVE_OUTPUT}" ]; then
-        OUTPUT_OPTION=" >/dev/null 2>&1"
-    fi
-    if [ -z "${_INVERT}" ]; then
-        ${TEST_FUNCTION}${OUTPUT_OPTION} && note "Passed: ${TEST_LABEL}" || error --noexit "Failed: ${TEST_LABEL}"
-    else
-        ${TEST_FUNCTION}${OUTPUT_OPTION} && error --noexit "Failed: ${TEST_LABEL}" || note "Passed: ${TEST_LABEL}"
-    fi
-}
+# Load the test-harness.sh file
+source "$(dirname $0)/test-harness.sh" || { echo "ERROR: test-harness.sh not found!" ;exit 1 ; }
 
 #TODO: Test the pretty output functions
 
