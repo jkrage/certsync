e0f0527355ab20401779d34b4d602ec5c59fa8cd
Joshua Krage <jkrage@guisarme.us>
2015-10-20 13:01:03 -0400

    Functioning improved test_wrapper
    Helper output functions overrridden
    to permit indenting or other distinction
    between harness output and routine output
    (including debug messages).

--- tests/test-harness.sh
+++ tests/test-harness.sh
@@ -4,12 +4,60 @@
 ### Included by other scripts, such as through:
 ### source "$(dirname $0)/test-harness.sh" || { echo "ERROR: test-harness.sh not found!" ;exit 1 ; }
 
+# Don't allow duplicate harness loading
+if [ ! -z "${__TEST_HARNESS_CANARY}" ]; then
+    echo "Test harness is already loaded. Not reloading."
+    return
+fi
+__TEST_HARNESS_CANARY=true
+
 # Load the helpers.sh file
 source "$(dirname $0)/../helpers.sh" || { echo "ERROR: helpers.sh not found!" ;exit 1 ; }
 # Enforce being in the tests directory
 [[ "$(dirname $0)" == "." ]] || { echo "ERROR: Please run this script directly from the tests/ directory." ; exit 1 ; }
 
+### Set up the testing environment
+### We'll override some helper functions to simplify output
+# cache_function_as original_function cached_function
+# Allows caching (copying) of a specified function in a new NICKNAME
+function cache_function_as () {
+  test -n "$(declare -f $1)" || return
+  eval "${_/$1/$2}"
+}
+
+# Cache the output functions
+cache_function_as debug _debug
+cache_function_as note _note
+cache_function_as warn _warn
+cache_function_as error _error
+
+# Define replacement functions for the test harness
+_INDENT="    "
+function debug () {
+    echo -n "${_INDENT}"
+    _debug "$@"
+}
+
+function note () {
+    echo -n "${_INDENT}"
+    _note "$@"
+}
+
+function warn () {
+    echo -n "${_INDENT}"
+    _warn "$@"
+}
+
+function error () {
+    _note --label="HELLO" "Test harness error called"
+    echo -n "${_INDENT}"
+    _error "$@"
+}
+
 ### Master wrapper function for Testing
+_LABEL_TEST="TESTING:"
+_LABEL_PASS="Passed:"
+_LABEL_FAIL="Failed:"
 function test_wrapper () {
     local _INVERT=""
     if [ "$1" == "--invert" ]; then
@@ -22,13 +70,13 @@ function test_wrapper () {
     local PRESERVE_OUTPUT=$3
     local OUTPUT_OPTION=""
 
-    debug "Testing: ${TEST_LABEL} using ${TEST_FUNCTION}"
+    _note --label="${_LABEL_TEST}" "${TEST_LABEL} using ${TEST_FUNCTION}"
     if [ ! -z "${PRESERVE_OUTPUT}" ]; then
         OUTPUT_OPTION=" >/dev/null 2>&1"
     fi
     if [ -z "${_INVERT}" ]; then
-        ${TEST_FUNCTION}${OUTPUT_OPTION} && note "Passed: ${TEST_LABEL}" || error --noexit "Failed: ${TEST_LABEL}"
+        ${TEST_FUNCTION}${OUTPUT_OPTION} && _note --label="${_LABEL_PASS}" "${TEST_LABEL}" || _error --noexit --label="${_LABEL_FAIL}" "${TEST_LABEL}"
     else
-        ${TEST_FUNCTION}${OUTPUT_OPTION} && error --noexit "Failed: ${TEST_LABEL}" || note "Passed: ${TEST_LABEL}"
+        ${TEST_FUNCTION}${OUTPUT_OPTION} && _error --noexit --label="${_LABEL_FAIL}" "${TEST_LABEL}" || _note --label="${_LABEL_PASS}" "${TEST_LABEL}"
     fi
 }
